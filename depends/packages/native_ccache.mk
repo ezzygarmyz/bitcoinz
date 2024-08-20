70e9497d4777ab4ad0b5d90028101d4761349132
ezzygarmyz <dragkhai@gmail.com>
2024-08-20 05:04:44 +0200

    test

--- depends/packages/native_ccache.mk
+++ depends/packages/native_ccache.mk
@@ -1,15 +1,28 @@
 package=native_ccache
-$(package)_version=3.7.12
+$(package)_version=4.10.2
 $(package)_download_path=https://github.com/ccache/ccache/releases/download/v$($(package)_version)
 $(package)_file_name=ccache-$($(package)_version).tar.gz
-$(package)_sha256_hash=d2abe88d4c283ce960e233583061127b156ffb027c6da3cf10770fc0c7244194
+$(package)_sha256_hash=108100960bb7e64573ea925af2ee7611701241abb36ce0aae3354528403a7d87
+$(package)_build_subdir=build
+$(package)_dependencies=native_cmake native_fmt native_xxhash native_zstd
 
 define $(package)_set_vars
-$(package)_config_opts=
+$(package)_config_opts += -DCMAKE_BUILD_TYPE=Release
+$(package)_config_opts += -DDEPS=LOCAL
+$(package)_config_opts += -DCMAKE_PREFIX_PATH=$(build_prefix)
+$(package)_config_opts += -DFMT_LIBRARY=$(build_prefix)/lib/libfmt.a
+$(package)_config_opts += -DXXHASH_LIBRARY=$(build_prefix)/lib/libxxhash.a
+$(package)_config_opts += -DZSTD_LIBRARY=$(build_prefix)/lib/libzstd.a
+$(package)_config_opts += -DREDIS_STORAGE_BACKEND=OFF
+$(package)_config_opts += -DENABLE_TESTING=OFF
+endef
+
+define $(package)_preprocess_cmds
+  mkdir $($(package)_build_subdir)
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_cmake) .. $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
